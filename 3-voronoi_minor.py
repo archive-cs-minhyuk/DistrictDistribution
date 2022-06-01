3742e2a1cf1afef99319cdf7db5536f08b6a9528
Minhyuk Song <76414054+archive-cs-minhyuk@users.noreply.github.com>
2022-06-01 18:42:08 +0900

    Add files via upload

---
+++ 3-voronoi_minor.py
@@ -0,0 +1,210 @@
+from sklearn.linear_model import LinearRegression
+import geopandas as gpd
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+import numpy as np
+import math
+import argparse
+import os
+
+#arguments
+parser = argparse.ArgumentParser()
+parser.add_argument('--adm1', default="Gyeongbuk",help='give adm1 level, default = Gyeongbuk')
+parser.add_argument('--adm2', default="Yeongju-si",help='give adm2 level, default = "Yeongju-si')
+parser.add_argument('--dir',default='Alphas_minor/',help='directory path for output .csv file')
+
+args = parser.parse_args()
+my_province = args.adm1
+my_city = args.adm2
+result_dir = args.dir
+
+if my_province is None:
+	print('Please give adm1')
+	exit(0)
+
+citycsv = pd.read_csv('./District_info/'+my_province+'/'+my_city+'.csv')
+citycsv = citycsv[['gid','lbl','val','geometry']]
+
+areaid_list = []
+areapop_list = []
+arealbl_list = []
+geom_list = []
+    
+for i in range(len(citycsv)):
+    areaid = citycsv.iloc[i]['gid']
+    areaid_list.append(areaid)
+
+    areapop = citycsv.iloc[i]['val']
+    areapop_list.append(areapop)
+        
+    arealbl = citycsv.iloc[i]['lbl']
+    arealbl_list.append(arealbl)
+        
+    geom = citycsv.iloc[i]['geometry']
+    geom = geom[10:-2]
+    geom_list.append(geom)
+
+areaid_geom_dict = dict(zip(areaid_list,geom_list))
+areaid_pop_dict = dict(zip(areaid_list,areapop_list))
+areaid_lbl_dict = dict(zip(areaid_list,arealbl_list))
+
+center_list = []
+for areaid in areaid_list:
+    city_geom = areaid_geom_dict [areaid]
+    tempgeom = city_geom.split(',')
+    left_top = tempgeom[1]
+    right_bot = tempgeom[3]
+    
+    center_point = ((float(left_top.split(' ')[1])+float(right_bot.split(' ')[1]))/2, (float(left_top.split(' ')[2])+float(right_bot.split(' ')[2]))/2)
+    center_list.append(center_point)
+areaid_center_dict = dict(zip(areaid_list,center_list))
+
+corepoints = pd.read_csv('./voronoi_result/'+my_province+'_'+my_city+'.csv')
+corepoints = corepoints[['lon','lat']]
+corepoints.index = corepoints.index + 1
+corepoints['corenum'] = corepoints.index
+
+#final_csv: informations about each grid's core_idx
+#This is the part where actual voronoi is performed!
+final_csv = pd.DataFrame(columns=["core_idx",'core_lat','core_lon','grid','pop'],dtype='float')
+for grid in areaid_center_dict:
+    df_temp = corepoints
+    gridlat = areaid_center_dict[grid][1]
+    gridlon = areaid_center_dict[grid][0]
+    df_temp['lon_diff'] = df_temp['lon'].apply(lambda x : x-gridlon)
+    df_temp['lat_diff'] = df_temp['lat'].apply(lambda x : x-gridlat)
+    df_temp['distance'] = df_temp['lon_diff']**2 + df_temp['lat_diff']**2
+    df_temp = df_temp.sort_values(by='distance')
+    df_temp = df_temp.reset_index(drop=True)
+    core_idx = df_temp.loc[0,"corenum"]
+    core_lat = df_temp.loc[0,"lat"]
+    core_lon = df_temp.loc[0,"lon"]
+    my_grid = grid
+    my_pop = areaid_pop_dict[grid]
+    new_data = {'core_idx':core_idx,'core_lat':core_lat,'core_lon':core_lon,'grid':my_grid,'pop':my_pop}
+    final_csv = final_csv.append(new_data,ignore_index=True)
+final_csv = final_csv.sort_values(by='core_idx')
+final_csv = final_csv.reset_index(drop=True)
+
+#coredf: informations about core num - population for each core
+final_csv = final_csv.fillna(0)
+cores = final_csv['core_idx'].tolist()
+cores_list = list(set(cores))
+coredf = pd.DataFrame(columns=['core','pop'], dtype=object)
+for core in cores_list:
+    temp = final_csv[final_csv['core_idx']==core]
+    tempsum = temp['pop'].sum()
+    new_data = {'core':[int(core)], 'pop':[tempsum]}
+    new_df = pd.DataFrame(new_data)
+    coredf= pd.concat([coredf,new_df])
+coredf = coredf.sort_values(by='pop',ascending=False)
+coredf = coredf.reset_index(drop=True)
+coredf.index = coredf.index + 1
+
+#calculating alphas
+def give_me_figures(dataset):
+    fig = plt.figure()
+    fig.subplots_adjust(hspace=0.2, wspace=0.4)
+
+    x=dataset.index
+    y=dataset['pop']
+
+    lny = []
+    for i in range(1,len(y)+1):
+        lny.append(math.log1p(y[i]))
+    lny = pd.DataFrame(lny)
+    lnx = []
+    for i in range(0,len(x)):
+        lnx.append(math.log(x[i]))
+    lnx = pd.DataFrame(lnx)
+
+    plt.subplot(2, 2, 1)
+    ax = sns.scatterplot(np.squeeze(lnx.values.tolist(),axis=1),np.squeeze(lny.values.tolist(),axis=1), color='tomato')
+    line_fitter = LinearRegression(fit_intercept=True)
+    line_fitter.fit((lnx).values.reshape(-1,1),lny)
+    mya = line_fitter.coef_
+    myb = line_fitter.intercept_
+    myalpha = -1/mya[0][0]
+    print("first")
+    print(myalpha)
+    plt.plot(np.squeeze(lnx.values.tolist(),axis=1),mya*lnx.values.reshape(-1,1) + myb)
+    first_y = np.squeeze(lny.to_numpy(),axis=1)
+    first_pred = np.squeeze(mya*lnx.values.reshape(-1,1) + myb)
+    ax.set_title("lns = b - 1/alpha * lnr", size = 11, fontweight='bold')
+    ax.set_xlabel("Ranking", size = 8)
+    ax.set_ylabel("Population", size = 8)
+
+    plt.subplot(2, 2, 2)
+    ax = sns.scatterplot(x, y, color='tomato')
+    line_fitter_2 = LinearRegression(fit_intercept=True)
+    line_fitter_2.fit(x.values.reshape(-1,1),y)
+    mya_2 = line_fitter_2.coef_
+    myb_2 = line_fitter_2.intercept_
+    myalpha_2 = -1/mya_2[0]
+    print("second")
+    print(mya_2[0])
+    plt.plot(x,mya_2*(x.values.reshape(-1,1))+myb_2)
+    ax.set_xlabel("Ranking", size = 8)
+    ax.set_ylabel("Population", size = 8)
+    
+    plt.subplot(2,2,3)
+    ax = sns.scatterplot(x, np.squeeze(lny.values.tolist(),axis=1), color='tomato')
+    line_fitter_3 = LinearRegression(fit_intercept=True)
+    line_fitter_3.fit(x.values.reshape(-1,1),lny)
+    mya_3 = line_fitter_3.coef_
+    myb_3 = line_fitter_3.intercept_
+    myalpha_3 = -1/mya_3[0]
+    print("third")
+    print(mya_3[0][0])
+    print(myb_3)
+    plt.plot(x,mya_3*(x.values.reshape(-1,1))+myb_3)
+    ax.set_xlabel("Ranking", size = 8)
+    ax.set_ylabel("ln(Population)", size = 8)
+
+    fig.set_figheight(8)
+    fig.set_figwidth(9)
+    return (myalpha, mya_2[0], mya_3[0][0])
+
+mpoplist = coredf['pop'].tolist()
+mindexlist = coredf.index.tolist()
+myilist = mindexlist[:-2]
+
+def MSE(y,pred) :
+    return np.mean(np.square(y-pred))
+
+def give_me_MSE(xlist, ylist):
+    if len(xlist)<=2:
+        return 0
+    line_fitter = LinearRegression()
+    xlist = np.array(xlist)
+    line_fitter.fit(xlist.reshape(-1,1),ylist)
+    mya = line_fitter.coef_
+    myb = line_fitter.intercept_
+
+    first_y = np.array(ylist)
+    first_pred = np.squeeze(mya*(xlist.reshape(-1,1))+myb,axis=1)
+    return MSE(first_y,first_pred)
+    
+anslist = []
+for i in mindexlist:
+    if i == len(mindexlist):
+        continue
+    yheadlist = list(map(math.log1p,mpoplist[1:i]))
+    xheadlist = list(mindexlist[1:i])
+    ytaillist = list(map(math.log1p,mpoplist[i:-1]))
+    xtaillist = list(mindexlist[i:-1])
+    anslist.append(give_me_MSE(xheadlist,yheadlist) + give_me_MSE(xtaillist,ytaillist))
+chp =anslist.index(min(anslist))
+criteria = chp
+living_grids = coredf.loc[:criteria]
+
+before_split = give_me_figures(coredf)
+after_split = give_me_figures(living_grids)
+
+alphas = pd.DataFrame(columns=["prov","minor","log_before",'notlog_before','log_after','notlog_after'],dtype='float')
+new_alphaset = {"prov":my_province,"minor":my_city,'log_before':before_split[0],'notlog_before':before_split[1],'log_after':after_split[0],'notlog_after':after_split[1]}
+alphas = alphas.append(new_alphaset,ignore_index = True)
+if not os.path.isdir(result_dir):
+	os.mkdir(result_dir)
+alphas.to_csv(result_dir+my_province+'_'+my_city+".csv",mode='w')
